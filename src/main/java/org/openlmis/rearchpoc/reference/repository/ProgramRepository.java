327467d7153f3c01ccffb1d7d68a4a4320c7695a
Chongsun Ahn <cahn922@yahoo.com>
2016-04-14 10:16:52 -0700

    Add query methods to program repository
    Also set config to show SQL.

--- src/main/java/org/openlmis/rearchpoc/reference/repository/ProgramRepository.java
+++ src/main/java/org/openlmis/rearchpoc/reference/repository/ProgramRepository.java
@@ -8,4 +8,33 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProgramRepository extends PagingAndSortingRepository<Program, Long> {
+
+    @Query(value = "SELECT DISTINCT p.*" + 
+            " FROM programs p" + 
+            " INNER JOIN programs_supported ps ON p.id = ps.programId" + 
+            " INNER JOIN role_assignments ra ON ra.programId = p.id" + 
+            " INNER JOIN role_rights rr ON rr.roleId = ra.roleId" + 
+            " WHERE p.active = TRUE" + 
+            " AND ps.active = TRUE" + 
+            " AND ra.userId = :userId" + 
+            " AND ps.facilityId = :facilityId" + 
+            " AND rr.rightName = ANY(('{'||:commaSeparatedRights||'}')\\:\\:VARCHAR[])" +
+            " AND ra.supervisoryNodeId IS NULL",
+            nativeQuery = true
+    )
+    List<Program> findByUserHomeFacility(@Param("userId") Long userId,
+                                         @Param("facilityId") Long facilityId,
+                                         @Param("commaSeparatedRights") String commaSeparatedRights);
+
+    @Query(value = "SELECT DISTINCT p.*" + 
+            " FROM programs p" + 
+            " INNER JOIN role_assignments ra ON p.id = ra.programId" + 
+            " INNER JOIN role_rights rr ON ra.roleId = rr.roleId" + 
+            " WHERE p.active = TRUE" + 
+            " AND ra.userId = :userId" +
+            " AND rr.rightName = ANY(('{'||:commaSeparatedRights||'}')\\:\\:VARCHAR[])" +
+            " AND ra.supervisoryNodeId IS NOT NULL",
+            nativeQuery = true)
+    List<Program> findByUserSupervisedFacilities(@Param("userId") Long userId,
+                                                 @Param("commaSeparatedRights") String commaSeparatedRights);
 }
