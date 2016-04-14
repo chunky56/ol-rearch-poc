7068f989561d33bbee0c85138698dac832b662bd
Chongsun Ahn <cahn922@yahoo.com>
2016-04-14 15:13:00 -0700

    Add query method to get user-based shipments
    Add query method to get shipments based on user, program and shipment status.

--- src/main/java/org/openlmis/rearchpoc/shipping/repository/ShipmentRepository.java
+++ src/main/java/org/openlmis/rearchpoc/shipping/repository/ShipmentRepository.java
@@ -1,7 +1,29 @@
 package org.openlmis.rearchpoc.shipping.repository;
 
 import org.openlmis.rearchpoc.shipping.domain.Shipment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ShipmentRepository extends PagingAndSortingRepository<Shipment, Long> {
+
+    @Query(value = "SELECT o.*" +
+            " FROM orders o" +
+            "   INNER JOIN requisitions r ON r.id = o.id" +
+            "   INNER JOIN supply_lines s ON o.supplyLineId = s.id" +
+            " WHERE r.programId = :programId" +
+            "   AND supplyingFacilityId IN (SELECT facilityId" +
+            "     FROM fulfillment_role_assignments" +
+            "       INNER JOIN role_rights rr ON rr.rightName = 'MANAGE_POD'" +
+            "     WHERE userId = :userId" +
+            "   GROUP BY facilityId)" +
+            "   AND o.status = ANY(('{'||:commaSeparatedStatuses||'}')\\:\\:VARCHAR[])",
+            nativeQuery = true
+    )
+    List<Shipment> findByUserProgramStatus(@Param("userId") Long userId, 
+                                           @Param("programId") Long programId, 
+                                           @Param("commaSeparatedStatuses") String commaSeparatedStatuses);
+
 }
